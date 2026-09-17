05ec2e8902e1727ea92d5ac90fc3d96ac5589051
Vaishali Reddy Toom <vaishalireddytoom@Vaishalis-MacBook-Air.local>
2026-09-17 21:30:58 +0530

    Update LifeTwin AI

--- run.py
+++ run.py
@@ -1,6 +1,8 @@
 from app import create_app
+import os
 
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(debug=True, host="127.0.0.1", port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=False, host="0.0.0.0", port=port)
